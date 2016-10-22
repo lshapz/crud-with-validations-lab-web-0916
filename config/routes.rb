7b4791ec323a837e84e50eafd5feb1ac89aae859
lshapz <laurashapiro01@gmail.com>
2016-10-22 15:20:52 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,31 @@
 Rails.application.routes.draw do
+  get 'songs/index'
+
+  get 'songs/edit'
+
+  get 'songs/update'
+
+  get 'songs/new'
+
+  get 'songs/create'
+
+  get 'songs/delete'
+
+  get 'songs/show'
+
+  get 'songs/index'
+
+  get 'songs/edit'
+
+  get 'songs/update'
+
+  get 'songs/new'
+
+  get 'songs/create'
+
+  get 'songs/delete'
+
+  get 'songs/show'
+
+  resources :songs
 end
